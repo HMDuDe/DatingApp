8180d61d34c3d187e7fd83020d71418910486706
HMDuDe <hughymartin21@gmail.com>
2022-08-01 22:14:53 +0200

    Expanded appuser entity properties

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -14,5 +14,25 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; } // TO-DO: Refactor to use enum
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }// TO-DO: Refactor to use enum
+
+        public string Interests { get; set; }
+
+        public string City { get; set; } // TO-DO: Use an Address object (will implement later)
+
+        public ICollection<Photo> Photos { get; set; }
     }
 }
