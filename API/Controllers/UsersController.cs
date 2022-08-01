bc08026ee6e47639f644afc1a2794d0dcbeb408b
HMDuDe <hughymartin21@gmail.com>
2022-08-01 23:37:47 +0200

    Added user repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,17 +14,16 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
         {
-            _context = context;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => Ok(await _userRepository.GetUsersAsync());
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUser(string username) => await _userRepository.GetUserByUsernameAsync(username);
     }
 }
