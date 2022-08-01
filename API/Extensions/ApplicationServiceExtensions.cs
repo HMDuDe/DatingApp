c2b3abb8fa67be5df47c8a9f51fcb335d7d2d16c
HMDuDe <hughymartin21@gmail.com>
2022-08-02 00:40:14 +0200

    Implementing auto mapping

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -6,6 +6,8 @@ using API.Data;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using API.Helpers;
 
 namespace API.Extensions
 {
@@ -15,6 +17,7 @@ namespace API.Extensions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => 
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
