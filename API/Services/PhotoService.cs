bafead478522bb35a3b706bf00f27a7d68533d97
HMDuDe <hughymartin21@gmail.com>
2022-08-04 22:06:21 +0200

    Extended user controller to provide upload capabilities to our API

--- API/Services/PhotoService.cs
+++ API/Services/PhotoService.cs
@@ -34,7 +34,7 @@ namespace API.Services
             {
                 using var stream = file.OpenReadStream();
                 
-                var uploadParams = new ImageUploadParams()
+                var uploadParams = new ImageUploadParams
                 {
                     File = new FileDescription(file.FileName, stream),
                     Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
